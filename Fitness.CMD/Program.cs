6134050f38f3a3ea46492575638a6a7f3f35a6d5
Egorov163 <Aleksandr_egorov_07@mail.ru>
2024-09-26 20:57:21 +0400

    Создана первичная реализация добавления и сохранения пользователя

--- Fitness.CMD/Program.cs
+++ Fitness.CMD/Program.cs
@@ -1,9 +1,32 @@
-﻿namespace Fitness.CMD
+﻿using Fitness.BL.Controller;
+using System;
+
+namespace Fitness.CMD
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Вас приветствует приложение Fitness");
+
+            Console.WriteLine("Введите имя пользователя");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Введите пол");
+            var gender = Console.ReadLine();
+
+            Console.WriteLine("Введите дату рождения");
+            var birthDate = DateTime.Parse(Console.ReadLine()); //TODO: Переписать
+
+            Console.WriteLine("Введите вес");
+            var weight = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите рост");
+            var hight = double.Parse(Console.ReadLine());
+
+            var userController = new UserController(name, gender, birthDate, weight, hight);
+
+            userController.Save();
         }
     }
 }
