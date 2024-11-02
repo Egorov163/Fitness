2ee64a08b91c9b355e093b8cda9d3939acce2736
Egorov163 <Aleksandr_egorov_07@mail.ru>
2024-11-02 17:24:51 +0400

    Создана первоначальная реализация сохранения и загрузка пользователя

---
+++ Fitness.BL/Controller/UserController.cs
@@ -0,0 +1,53 @@
+﻿using Fitness.BL.Model;
+using System.Text.Json;
+
+namespace Fitness.BL.Controller
+{
+    /// <summary>
+    /// Контроллер пользователя.
+    /// </summary>
+    public class UserController
+    {
+        /// <summary>
+        /// Пользователь приложения.
+        /// </summary>
+        public User User { get; }
+        /// <summary>
+        /// Создание нового контроллера пользователя.
+        /// </summary>
+        /// <param name="user"> Имя нового пользователя </param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public UserController(string userName, string genderName, DateTime birthDay, double weight, double height)
+        {
+            // TODO: Проверка
+
+            var gender = new Gender(genderName);
+            User = new User(userName, gender, birthDay, weight, height);
+        }
+        public UserController()
+        {
+            using (FileStream fs = new FileStream("users.dat", FileMode.OpenOrCreate))
+            {
+                if (JsonSerializer.Deserialize<User>(fs) is User user)
+                {
+                    User = user;
+                }
+                // TODO: Что делать если пользователя не прочитали
+            }
+        }
+        /// <summary>
+        /// Сохранить данные пользователя.
+        /// </summary>
+        public void Save()
+        {
+            using (FileStream fs = new FileStream("users.dat", FileMode.OpenOrCreate))
+            {
+                JsonSerializer.Serialize(fs, User);
+            }
+        }
+        /// <summary>
+        /// Получить данные пользователя.
+        /// </summary>
+
+    }
+}
